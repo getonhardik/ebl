9896a1f0a832e3eda1f425fc8caa94c8610c5f0c
getonhardik <hardik4hardik@gmail.com>
2017-02-17 18:36:14 +0530

    initial change

--- www/js/app.js
+++ www/js/app.js
@@ -8,9 +8,7 @@ angular.module('app', [
         .run(function ($ionicPlatform, $rootScope, $http, $ionicPopup) {
             $ionicPlatform.ready(function () {
                 // Hide the accessory bar by default
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
+               
                 if (window.StatusBar) {
                     // org.apache.cordova.statusbar required
                     StatusBar.styleDefault();

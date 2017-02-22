bcdd7419efb90a7c7a67359fb7ab7cb43986d477
getonhardik <hardik4hardik@gmail.com>
2017-02-22 13:23:16 +0530

    congig

--- www/js/app.js
+++ www/js/app.js
@@ -11,8 +11,7 @@ angular.module('app', [
                 /*if (window.cordova && window.cordova.plugins.Keyboard) {
                     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 }*/
-               
-                    navigator.splashscreen.hide();
+                
                 if (window.StatusBar) {
                     // org.apache.cordova.statusbar required
                     StatusBar.styleDefault();

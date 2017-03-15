80baf7ac2d7b3aa5ea50152df0e5986a098037d8
getonhardik <hardik4hardik@gmail.com>
2017-03-15 16:55:32 +0530

    cart 17031511

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1787,13 +1787,11 @@ angular.module('app.controllers', [])
                     product: p_id,
                     user_id: u_id,
                 };
-                alert(item_id);
-//                $scope.showLoading();
+                $scope.showLoading();
                 $rootScope.service.get('addwishlist', params, function (res) {
                     console.log(res);
-                    alert(res.status+' is '+res.message);
                     if (res.status == 'error') {
-//                        $scope.hideLoading(); 
+                        $scope.hideLoading(); 
                         $ionicPopup.alert(
                                 {
                                     title: 'Error',
@@ -1804,7 +1802,7 @@ angular.module('app.controllers', [])
                         return;
                     }
                     if (res.status == 'SUCCESS') {
-//                        $scope.hideLoading();                       
+                        $scope.hideLoading();                       
                         $scope.items_qty = res.items_qty;
                         var params = {
                             cart_item_id: item_id,
